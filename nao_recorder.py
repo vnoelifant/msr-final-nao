dec0e49a4d311e56c58c4ac92bb4fdcde9b7af7e
vnoelifant <veronicamedrano2019@u.northwestern.edu>
2019-07-21 20:44:47 -0500

    updated function details and more stub functions

--- nao_recorder.py
+++ nao_recorder.py
@@ -59,6 +59,10 @@ class SoundReceiverModule(naoqi.ALModule):
             self.wavfile = None;
             self.recording = False
             self.silence = 500 # threshold to detect silence 
+            # buffer for audio sound
+            self.audioBuffer = []
+            # buffer for silence
+            self.silenceBuffer = []
         except BaseException, err:
             print( "ERR: SoundReceiverModule: loading error: %s" % str(err));
 
@@ -114,8 +118,6 @@ class SoundReceiverModule(naoqi.ALModule):
         # set recording to stop
         self.recording = False
 
-
-
     def rawToWav(self,filename):
         #print("self.outfile",self.outfile)
         rawfile = filename + ".raw"
@@ -138,7 +140,6 @@ class SoundReceiverModule(naoqi.ALModule):
 
     """
     This is the method that receives all the sound buffers from the "ALAudioDevice" module
-    
     """
     def processRemote(self, nbOfChannels, nbrOfSamplesByChannel, aTimeStamp, buffer):
         # This method receives the streaming microphone data. The buffer parameter contains an 
@@ -150,23 +151,26 @@ class SoundReceiverModule(naoqi.ALModule):
    
         # reshape data
         aSoundData = np.reshape(aSoundDataInterlaced,(nbOfChannels, nbrOfSamplesByChannel), 'F');
+
+        # set a counter for silence duration
+        silent_cnt = 0
         # save to file
-        if(self.outfile == None):
-            strFilenameOut = "test.raw";
-            print( "Writing sound to '%s'" % strFilenameOut);
-            self.outfile = open( strFilenameOut, "wb");
+        # if(self.outfile == None):
+        #     strFilenameOut = "test.raw";
+        #     print( "Writing sound to '%s'" % strFilenameOut);
+        #     self.outfile = open( strFilenameOut, "wb");
      
-            for nNumChannel in range( 1, nbOfChannels ):
-                strFilenameOutChan = strFilenameOut.replace(".raw", "_%d.raw"%nNumChannel);
-                self.aOutfile[nNumChannel-1] = open( strFilenameOutChan, "wb");
-                print("Writing other channel sound to '%s'" % strFilenameOutChan);
+        #     for nNumChannel in range( 1, nbOfChannels ):
+        #         strFilenameOutChan = strFilenameOut.replace(".raw", "_%d.raw"%nNumChannel);
+        #         self.aOutfile[nNumChannel-1] = open( strFilenameOutChan, "wb");
+        #         print("Writing other channel sound to '%s'" % strFilenameOutChan);
             
-        #tofile: Write array to a file as text or binary (default).
-        # aSoundDataInterlaced.tofile(self.outfile ); # write 4 channels
-        aSoundData[0].tofile(self.outfile); # write only one channel
+        # #tofile: Write array to a file as text or binary (default).
+        # # aSoundDataInterlaced.tofile(self.outfile ); # write 4 channels
+        # aSoundData[0].tofile(self.outfile); # write only one channel
           
-        for nNumChannel in range(1, nbOfChannels):
-            aSoundData[nNumChannel].tofile(self.aOutfile[nNumChannel-1]); 
+        # for nNumChannel in range(1, nbOfChannels):
+        #     aSoundData[nNumChannel].tofile(self.aOutfile[nNumChannel-1]); 
         
         # conditions on detecting speech to be transcribed
         # sound below a set silence bound
@@ -175,38 +179,54 @@ class SoundReceiverModule(naoqi.ALModule):
         # sound is at peak value
         sound_peak = self.get_peak_sound(aSoundData)
         
-        # speech is detectedf if sound reached peak and below silence threshold 
-        # before and after speaking
-        speech_detected = sound_silent and sound_peak
-        
-        # if speech detected trim silence, add to buffer, and stop recording
+        # if sound reached its peak, keep silence count at 0
+        if sound_peak > 15000:
+            silent_cnt = 0
+            print "silence count",silent_cnt
+
+        # if sound is considered silent, add silence count
+        if sound_silent:
+            silent_cnt += 1
+            print "silence count",silent_cnt
+
+        # speech is detected if sound reached peak,is surrounded by silence, and silence
+        # is detected for over 10 counts
+        speech_detected = sound_silent and sound_peak and silent_cnt > 10
+
+         # if speech detected avg volume, trim silence, add to buffer, and stop recording 
         if speech_detected:
+            # average out the volume of data
+            self.avg_volume(aSoundData)
             # trim silence on both ends of data
-            self.trim_silence()
+            self.trim_silence(aSoundData)
             # add detected speech to sound buffer
-            self.add_to_buffer()
+            self.add_to_buffer(aSoundData)
             # stop recording 
             self.stop_recording()
+
+    def avg_volume(aSoundData):
+        pass
 
     def get_peak_sound(self,aSoundData):
         "Returns 'True' if max sound at peak value"
         return np.max(aSoundData) > 15000
     
     def get_silence(self, aSoundData):
-        "Returns 'True' if sound data below set 'silent' threshold"
+        "Returns 'True' if sound data below'silent' threshold"
         print len(aSoundData)
         print max(aSoundData)
         print type(aSoundData)
-        return max(aSoundData) < self.silence
+        self.silent_cnt += 1
+        return max(aSoundData) < self.silence 
 
-    def trim_silence():
+    def trim_silence(self,aSoundData):
         pass
 
-    def add_to_buffer():
-        pass
+    def add_to_buffer(self,aSoundData):
+        self.audioBuffer.append(aSoundData)
 
-    def clear_audio_buffer():
-        pass
+    def clear_audio_buffer(self):
+        self.audioBuffer = []
 
 
 
